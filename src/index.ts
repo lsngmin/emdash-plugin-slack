60d9e0dc3f940e5e5bd5eb34eb3d527351a991ed
sngmin <lsngmin@naver.com>
2026-04-22 05:02:39 +0900

    chore: bump version to 0.1.1

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import type { PluginDescriptor } from "emdash";
 export function slackPlugin(): PluginDescriptor {
 	return {
 		id: "emdash-plugin-slack",
-		version: "0.1.0",
+		version: "0.1.1",
 		format: "standard",
 		entrypoint: "emdash-plugin-slack/sandbox",
 		capabilities: ["network:fetch:any", "read:content"],
